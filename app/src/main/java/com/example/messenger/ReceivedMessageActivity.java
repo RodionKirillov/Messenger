b04ec3af994d8adbc667c666f442b0d74f4fb43e
RodionKirillov <rodion.44454@gmail.com>
2023-10-01 19:42:52 +0300

    Commit 3. Creating a Intent.

--- app/src/main/java/com/example/messenger/ReceivedMessageActivity.java
+++ app/src/main/java/com/example/messenger/ReceivedMessageActivity.java
@@ -2,7 +2,9 @@ package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceivedMessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ReceivedMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_received_message);
+
+        TextView textViewMessage = findViewById(R.id.textViewMessage);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+
+        textViewMessage.setText(message);
     }
 }
